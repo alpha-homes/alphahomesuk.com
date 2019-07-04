af92dd5e441887b29d38e60f13c733cfe89c47e7
Rai Butera <rai@rbutera.com>
2019-07-04 11:56:40 +0100

    added material design icons, google fonts, and installed buefy

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from "~/layouts/Default.vue";
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component("Layout", DefaultLayout);
+
+  // icons
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css"
+  });
+
+  // fonts
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://fonts.googleapis.com/css?family=Libre+Franklin:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i|Martel:200,300,400,600,700,800,900|Playfair+Display:400,400i,700&display=swap"
+  });
+
+  // fonts
 }
