2817db1718f5b032de9bf96f282d3562e0159cdc
Rai Butera <rai@rbutera.com>
2019-07-20 08:07:12 +0100

    upgraded all packages

---
+++ src/pages/services/guaranteed-rent.vue
@@ -0,0 +1,30 @@
+<template>
+  <Layout>
+    <div id="about-container">
+      <section class="hero is-large">
+        <div class="hero-body">
+          <div class="container content">
+            <h1>Guaranteed Rent</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+          </div>
+        </div>
+      </section>
+      <section class="section is-medium">
+        <div class="container content">
+          <h1>Test</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+        </div>
+      </section>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "About us"
+  }
+};
+</script>
